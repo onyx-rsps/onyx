9c6505365fbd639a056651e9deb127edc4fdc849
Kyle Escobar <82987787+kyleescobar@users.noreply.github.com>
2021-07-28 13:41:47 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.20"
+}
+
+group = "dev.onyx"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
