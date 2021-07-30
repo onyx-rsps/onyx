40e9cc6ef7652f2f5465d2340c9e433a7c40819c
Kyle Escobar <82987787+kyleescobar@users.noreply.github.com>
2021-07-30 00:51:44 -0400

    Add debug log message to game cycle.

--- server/engine/src/main/kotlin/dev/onyx/server/engine/Engine.kt
+++ server/engine/src/main/kotlin/dev/onyx/server/engine/Engine.kt
@@ -6,6 +6,10 @@ import dev.onyx.server.engine.model.World
 import dev.onyx.server.engine.net.NetworkServer
 import org.tinylog.kotlin.Logger
 import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.math.floor
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 class Engine : TimerTask() {
 
@@ -15,9 +19,18 @@ class Engine : TimerTask() {
     private var running = false
 
     /**
+     * Whether the game engine is currently pause.
+     */
+    var paused = false
+
+    /**
      * The current tick counter of the game engine.
      */
     var tick = 0L
+
+    private var cycleTime = 0
+
+    private var debugTickCounter = 0
 
     fun start() {
         if(running) return
@@ -56,6 +69,12 @@ class Engine : TimerTask() {
      * game engine components.
      */
     override fun run() {
+        if(paused) {
+            return
+        }
+
+        val start = System.currentTimeMillis()
+
         /*
          * Cycle the game world.
          */
@@ -65,5 +84,47 @@ class Engine : TimerTask() {
          * Increment the tick counter.
          */
         tick++
+
+        /*
+         * Calculate the time the cycle took to complete.
+         */
+        cycleTime += (System.currentTimeMillis() - start).toInt()
+
+        if(debugTickCounter++ >= TICKS_PER_DEBUG_LOG) {
+            val freeMemory = Runtime.getRuntime().freeMemory()
+            val totalMemory = Runtime.getRuntime().totalMemory()
+            val maxMemory = Runtime.getRuntime().maxMemory()
+
+            val cycleTimeMs = cycleTime / TICKS_PER_DEBUG_LOG
+            val playerCount = world.players.size
+            val usedMem = (totalMemory - freeMemory) / (1024 * 1024)
+            val allocMem = totalMemory / (1024 * 1024)
+            val totalMem = maxMemory / (1024 * 1024)
+
+            val debugMessage =  "[Cycle Time: $cycleTimeMs] " +
+                                "[Players Online: $playerCount] " +
+                                "[Memory Usage: used=${usedMem}MB / alloc=${allocMem}MB / total=${totalMem}MB]"
+
+            Logger.debug(debugMessage)
+
+            debugTickCounter = 0
+            cycleTime = 0
+        }
+
+        val freeTime = ServerConfig.tickRate - (System.currentTimeMillis() - start)
+        if(freeTime < 0) {
+            val timeBehind = freeTime.absoluteValue
+            val ticksBehind = (ServerConfig.tickRate / freeTime).toInt()
+            Logger.warn("Can't keep up! Is the server overloaded? Running ${timeBehind}ms or $ticksBehind ticks behind.")
+        }
+    }
+
+    companion object {
+
+        /**
+         * The number of engine game ticks before a debug log entry is written to
+         * the console and log files.
+         */
+        private const val TICKS_PER_DEBUG_LOG = 50
     }
 }
